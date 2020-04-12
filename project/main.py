d92381abb49a122daa6d0bab763b96e726ab1e9b
cshifrin <chris.shifrin7@gmail.com>
2020-04-12 14:33:09 -0400

    all

--- project/main.py
+++ project/main.py
@@ -5,14 +5,11 @@ import re
 
 app = Flask(__name__)
 
-#Create secret key
-app.secret_key = 'key'
-
 #Configure the application
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MTSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-ap.config['MYSQL_DB'] = 'BookStore'
+app.config['MYSQL_DB'] = 'BookStore'
 
 mysql = MySQL(app)
 
@@ -20,23 +17,27 @@ mysql = MySQL(app)
 @app.route('/BookStore/', methods=['GET', 'POST'])
 def login():
     return render_template('signin.html', msg='')
+    msg = ''
+    #Check if "email" and "password" POST requests exist
+    if request.method == 'POST' and 'inputEmail' in request.form and 'inputPassword' in request.form:
+        inputEmail = request.form['inputEmail']
+        inputPassword = request.form['inputPassword']
 
-msg = 'Error'
-#Check if "email" and "password" POST requests exist
-if request.method == 'POST' and 'Email' in request.form and 'Password' in request.form:
-    inputEmail4 = request.form['inputEmail4']
-    inputPassword4 = request.form['inputPassword4']
-
-#Check if account exists using MySQL
-cursor = mysql.connection.cursor(MySQL.cursors.DictCursor)
-cursor.execute('Select * FROM Users WHERE Email = %s AND Password = %s', (inputEmail4, inputPassword4,))
-account = cursor.fetchone()
+        #Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQL.cursors.DictCursor)
+        cursor.execute('Select * FROM Users WHERE Email = %s AND Password = %s', (inputEmail, inputPassword,))
+        user = cursor.fetchone()
 
 
-#Check if account exists using MySQL
-cursor = mysql.connection.cursor(MySQL.cursors.DictCursor)
-cursor.execute('SELECT * FROM Users WHERE Email = %s AND Password = %s', (inputEmail4, inputPassword4,))
-account = cursor.fetchone()
+        #Check if account exists in database
+        if account:
+            session['loggedin'] = True
+            session['inputEmail'] = user['inputEmail']
+            session['inputPassword'] = user['inputPassword']
+            return 'Logged in successfully.'
+        else:
+            msg = 'ERROR'
+    return render_template('home.html', msg=msg)
 
 #If account exists create session
 #if account:

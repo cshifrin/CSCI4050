cb7101437f1733f87f7616ed3829a652d36bfb2f
dsg37120 <45467892+dsg37120@users.noreply.github.com>
2020-04-15 00:11:47 -0400

    Add files via upload

--- project/app.py
+++ project/app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/home/')
+@app.route('/BookStore/')
 def home():
     return render_template('home.html')
 
-@app.route('/home/signin/')
-def about():
+@app.route('/BookStore/signin/')
+def signin():
     return render_template('signin.html')
+
+@app.route('/BookStore/forgotpwd/')
+def forgotpwd():
+    return render_template('forgotpwd.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
